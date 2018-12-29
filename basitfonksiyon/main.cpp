10a9e2f63f77fe2bcf0ddf73180f3c7d71cadc7f
EminCimen <46209839+EminCimen@users.noreply.github.com>
2018-12-29 06:23:44 +0300

    Add files via upload

--- basitfonksiyon/main.cpp
+++ basitfonksiyon/main.cpp
@@ -2,10 +2,16 @@
 
 int f(int x){
     printf("Fonksiyonun icindeki sayi %d\n",x);
+    return 7; // Fonksiyon işlemlerini tamamladıktan sonra 7 değerini döndürecek dolayısıyla ekranda x'ten sonra 7 basılacak
+}
+
+void g(int x){
+    printf("Fonksiyonun icindeki sayi %d\n",x);
 }
 
 int main()
 {
-   f(3);
-   f(550);
+   printf("%d\n",f(3)); // Ekran 3 ve 7 olmasını bekliyoruz.
+   g(8); // Void fonksiyonu değer döndürmediği için printf'in içinde basamıyoruz.
+   
 }
