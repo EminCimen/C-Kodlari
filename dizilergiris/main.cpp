7d8051650c41f4633afaea097dbe612d8e4b2f52
EminCimen <46209839+EminCimen@users.noreply.github.com>
2018-12-29 07:53:53 +0300

    Add files via upload

---
+++ dizilergiris/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    //DİZİ ELEMANLARINI KULLANICIDAN OKUMA
+    int a[5];
+    for(int i=0;i<5;i++){
+        printf("Lutfen bir sayi girin>");
+        scanf("%d",&a[i]);
+    }
+    
+    // EN BÜYÜK DEĞERİ BULMA
+    int eb=a[0];
+    for(int i=1;i<5;i++){
+        if(a[i]>eb)
+            eb=a[i];
+    }
+    // EN KÜÇÜK DEĞERİ BULMA
+    int ek=a[0];
+    for (int i=1;i<5;i++){
+        if(ek>a[i])
+            ek=a[i];
+    }
+    //DEĞERLERİ TOPLAMA
+    int toplam=0;
+    for(int i=0;i<5;i++){
+        
+        toplam+=a[i];
+    }
+    // ÇIKTILAR
+    printf("En buyuk sayi %d\n",eb);
+    printf("En kucuk sayi %d\n",ek);
+    printf("Toplamlari %d\n",toplam);
+    printf("Ortalamalari %d\n",toplam/5);
+}
